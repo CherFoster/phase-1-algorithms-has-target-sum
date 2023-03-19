06d5784bc0e090fdb88e61aac0bae8fb57f30699
Cherise Foster <cherise.foster@outlook.com>
2023-03-19 14:24:41 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(array, target) { 
+  for (let i = 0; i < array.length; i++){
+    for (let j = i + 1; j < array.length; j++){
+      if (array[i] + array[j] === target){
+        return true;
+      } 
+    } 
+  } return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+ O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+  hasTargetSum takes an array and a target number as arguments
+    iterate through the array 
+      if two numbers are equal to the target number
+        return true
+      else return false
+
 */
 
 /*
-  Add written explanation of your solution here
+  Function hasTargetSum takes an array and a target as arguments. 
+  If two of the numbers in the array add up to the target number, 
+  the function will return true. If not, it returns false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,12 +33,11 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([0, 2, 9], 15))
 
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
+  console.log("=>", hasTargetSum([0, 1, 2, 3, 4, 5], 6))
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
